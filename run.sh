7c23abe974768757008b6d2bb7803eaf28872e01
wuyao1994 <wuyaoiot@163.com>
2018-10-19 01:19:33 +0800

    fix issue

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ mvn clean install -Dmaven.test.skip=true
 
 # Export the active docker machine IP
 export DOCKER_IP=$(docker-machine ip $(docker-machine active))
-export ZOOKEEPER_IP=172.17.118.141
+export ZOOKEEPER_IP=192.168.0.114
 # docker-machine doesn't exist in Linux, assign default ip if it's not set
 DOCKER_IP=${DOCKER_IP:-0.0.0.0}
 
@@ -16,6 +16,8 @@ docker-compose stop
 docker-compose rm -f
 
 # Start the config service first and wait for it to become available
+docker-compose up -d zookeeper
+docker-compose up -d kafka
 docker-compose up -d config-service
 
 while [ -z ${CONFIG_SERVICE_READY} ]; do
