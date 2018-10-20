9f28478fa8840b48dbcd00a7950101ef85d7827c
wuyao1994 <wuyaoiot@163.com>
2018-10-20 23:36:50 +0800

    change docker-compose

--- run.sh
+++ run.sh
@@ -10,9 +10,7 @@ export DOCKER_IP=$(docker-machine ip $(docker-machine active))
 # docker-machine doesn't exist in Linux, assign default ip if it's not set
 DOCKER_IP=${DOCKER_IP:-0.0.0.0}
 
-# Remove existing containers
-docker-compose stop
-docker-compose rm -f
+docker-compose down
 
 # Start the config service first and wait for it to become available
 docker-compose up -d zookeeper
