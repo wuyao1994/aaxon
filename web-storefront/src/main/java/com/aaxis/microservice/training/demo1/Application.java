83ef27470c4917e852067f76d6fed6897f8488a3
wuyao1994 <wuyaoiot@163.com>
2018-10-21 14:32:55 +0800

    fix pom issue

--- web-storefront/src/main/java/com/aaxis/microservice/training/demo1/Application.java
+++ web-storefront/src/main/java/com/aaxis/microservice/training/demo1/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
  * @author elvis
  */
 @SpringCloudApplication
+@EnableHystrix
 @RefreshScope
 public class Application {
 	@Bean
